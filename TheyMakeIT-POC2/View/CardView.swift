5452d48d692bfbaf7639bf6deb7cb7746446419a
Sébastien Jarjanette <sebastien.jarjanette@icloud.com>
2024-11-07 17:34:51 +0100

    Update style cards

--- TheyMakeIT-POC2/View/CardView.swift
+++ TheyMakeIT-POC2/View/CardView.swift
@@ -11,23 +11,25 @@ struct CardView: View {
     let content : [String]
     let width : CGFloat
     let pictoName : String
-  
+    
     
     var body: some View {
         VStack {
-                    Image(systemName: pictoName)
-                        .padding(.bottom, 4)
-                    ForEach(content, id: \.self) { text in
-                        Text(text)
-                            .font(.callout)
-                    }
-                }
-                .frame(width: width, height: 100)
-                .padding(4)
-                .background(RoundedRectangle(cornerRadius: 10)
-                    .stroke(.purple, lineWidth: 2))
+            Image(systemName: pictoName)
+                .padding(.bottom, 4)
+            ForEach(content, id: \.self) { text in
+                Text(text)
+                    .font(.callout)
+                    .multilineTextAlignment(.center)
+
             }
+        }
+        .frame(width: width, height: 100)
+        .padding(4)
+        .background(RoundedRectangle(cornerRadius: 10)
+            .stroke(.purple, lineWidth: 2))
     }
+}
 
 
 #Preview {
